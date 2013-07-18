22a2ebacc07dc99bb9ecfcfa5dbf9b47004318df
Apprentice <apprentice@dbc15.local>
2013-07-17 21:28:07 -0700

    last items done

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-
+  number_list();
   $(function() {
     $('#sortable').sortable({
       placeholder: "ui-state-highlight"
-
     });
     $('#sortable').disableSelection();
   });
 
+  $('.btn').click(function(){
+    $.blockUI();
+    $('.blockOverlay').attr('title','Click to unblock').click($.unblockUI); 
+  })
 
   $('#sortable').mouseup(function(){
     sorted_list_ids = []
@@ -20,18 +23,21 @@ $(document).ready(function() {
       }
       $.post('/change_list', { sorted_list: sorted_list_ids });
     });
-    number_list();
   });
-
-  // $('.list').mouseup(function(){
-       
-  // })
-
-
-
-
+  $('#sortable').mouseleave(function(){
+    number_list();
+  })
 });
 
 function number_list(){
-  $('#sortable li').each().text('1 - ')
+  var counter = 1
+  $('#sortable li').each(function(index, value){
+    $(value).find('#counter').remove();
+    $(value).prepend('<span id=counter>' + counter + ' - ' + '</span>');
+    counter +=1
+  });
 }
+
+
+
+
